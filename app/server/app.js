c9e1dbef28743a3519873c2b7586bdcd406cf9f8
stuart <stuartcoope@gmail.com>
2013-12-18 13:25:48 +0000

    Ping Pong

--- app/server/app.js
+++ app/server/app.js
@@ -50,14 +50,45 @@ server.listen(3003);
 
 app.use(express.static(path.join(__dirname, '../public')));
 
+
+var pingDue = true;
+var score = 0;
+
+var broadcastScore = function(score){
+	io.sockets.emit('score', { score: score});
+	console.log(score);
+};
+
+var pingPong = function(type){
+
+	if(type == 'ping' && pingDue){
+		score++;
+	}else if(type == 'pong' && !pingDue){
+		score++;
+	}else{
+		score = 0;
+	}
+
+	if(type == 'ping'){
+		pingDue = false;
+	}else{
+		pingDue = true;
+	}
+
+	broadcastScore(score);
+
+};
+
 io.sockets.on('connection', function (socket) {
   
   socket.on('ping', function (data) {
     socket.broadcast.emit('ping', {});
+		pingPong('ping');
   });
 
   socket.on('pong', function (data) {
     socket.broadcast.emit('pong', {});
+		pingPong('pong');
   });
 
 });
